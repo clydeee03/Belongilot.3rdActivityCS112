dcb399c21ffd6e24775db3735edff59f91d310f8
clydeee03 <144906657+clydeee03@users.noreply.github.com>
2023-09-27 05:35:07 -0700

    Add files via upload

---
+++ Belongilot_secondtominuteconverterActivity.py
@@ -0,0 +1,4 @@
+variableSecond = eval(input("Enter seconds here:"))
+variableMinute = variableSecond // 60
+variableMinute1 = variableSecond % 60
+print(variableSecond, "seconds is", variableMinute, "minute/s and", variableMinute1, "seconds" )
